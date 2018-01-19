4fdbfd87a0307e73d71f7522b4097abe514ce87d
pranishg <34614328+pranishg@users.noreply.github.com>
2018-01-20 02:38:43 +0545

    Create creds.py

---
+++ creds.py
@@ -0,0 +1,35 @@
+# my favorite blogs on steemit
+top_writers = ["pranishg","upvotebaby","ashishfromnepal","kshitizraj"]
+
+# add my favorites 
+my_favorites = []
+
+my_subscriptions = top_writers  + my_favorites
+
+accounts = {"upvotebaby" : {"posting_key":"5JqCPv114KBhseh9iCmVaHPRgnCpyYoQBQyJ4jhZuBKw8V6qaqW", 
+                             "active_key":"STM7ZnhAg2SCdL1rghTBSkqT4suhXX2PX9oRw68a5Zi778SrDoC4i"},
+            "paruz" : {"posting_key":"5JHcQHXt61ePUwaYSiU4URPfH1xj8s8Xts2U7JQsWPqDT3NqyJn",
+                         "active_key":"STM5iHDeJV3oBxGrpAMkVtSfJxXMEM6fNeh8ec4h1LEdeXdUsPiYK"},
+"bholapuri" : {"posting_key":"5JmCT2UCHAKSdA1nJZ83Z3zJcLzpYhbtAwSLCwTZR1kxKqPR5b2",
+                         "active_key":"STM5iHDeJV3oBxGrpAMkVtSfJxXMEM6fNeh8ec4h1LEdeXdUsPiYK"},
+"ashishfromnepal" : {"posting_key":"5HtQ1GvBj8wyspnxSHDYETWZWjJwjChX89HbCyiH6npvaN4Jf19",
+                         "active_key":"STM5iHDeJV3oBxGrpAMkVtSfJxXMEM6fNeh8ec4h1LEdeXdUsPiYK"},
+
+"kshitizraj" : {"posting_key":"5JL2STT4PSNHgVdXK5AEpVRzz11QMEV7fmiUup5GMU56eienawr",
+                         "active_key":"STM5iHDeJV3oBxGrpAMkVtSfJxXMEM6fNeh8ec4h1LEdeXdUsPiYK"},
+
+"kshitizrun" : {"posting_key":"5HyCuzBXUVLU9RAD9TcRQt5jwSdEp6dYBtwBD6zY9k8cQ9i6t6C",
+                         "active_key":"STM5iHDeJV3oBxGrpAMkVtSfJxXMEM6fNeh8ec4h1LEdeXdUsPiYK"},
+
+"upvotebabyrun" : {"posting_key":"5JMd7LM2YtuS7U2nYbxhNrgQKtbdgdR7t92khk182mNJNei9CmY",
+                         "active_key":"STM5iHDeJV3oBxGrpAMkVtSfJxXMEM6fNeh8ec4h1LEdeXdUsPiYK"},
+
+"anujamaharjan" : {"posting_key":"5KbirS1HtZFnjEi26fLdrpzvgBKaAQTeUfTD8Mt7HZE4oJk9gXE",
+                         "active_key":"STM5iHDeJV3oBxGrpAMkVtSfJxXMEM6fNeh8ec4h1LEdeXdUsPiYK"},
+
+"sadiksha" : {"posting_key":"5KMhkY76i7Shy7my5NASfgYCwdD6oCmDt2F5XVwMSp5GJXGXzri",
+                         "active_key":"STM5iHDeJV3oBxGrpAMkVtSfJxXMEM6fNeh8ec4h1LEdeXdUsPiYK"}
+
+
+            }
+
